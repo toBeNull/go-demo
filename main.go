1b8e828c23e43b23a9d1adaa49b9499f2344a9a3
pengjian <pengjian.cool@bytedance.com>
2022-07-12 10:57:34 +0800

    env

--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	"go-hello/version"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
+	addr := ":8080"
 	startTime := time.Now()
 	appVersion := version.AppVersion()
 	format := "2006.01.02 15:04:05"
+
+	nodeName, _ := os.LookupEnv("MY_NODE_NAME")
+	nodeIp, _ := os.LookupEnv("MY_NODE_IP")
+	podName, _ := os.LookupEnv("MY_POD_NAME")
+	podIp, _ := os.LookupEnv("MY_POD_IP")
+	namespace, _ := os.LookupEnv("MY_POD_NAMESPACE")
+	serviceAccountName, _ := os.LookupEnv("MY_POD_SERVICE_ACCOUNT")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("[%v] receive request... \n", time.Now().Format(format))
@@ -20,9 +29,21 @@ func main() {
 		fmt.Fprintln(w, "version    : ", appVersion)
 		fmt.Fprintln(w, "startTime  : ", startTime.Format(format))
 		fmt.Fprintln(w, "currentTime: ", time.Now().Format(format))
+		fmt.Fprintln(w, "")
+
+		fmt.Fprintln(w, "host name  : ", nodeName)
+		fmt.Fprintln(w, "host ip    : ", nodeIp)
+
+		fmt.Fprintln(w, "pod name   : ", podName)
+		fmt.Fprintln(w, "pod ip     : ", podIp)
+
+		fmt.Fprintln(w, "namespace  : ", namespace)
+		fmt.Fprintln(w, "service account : ", serviceAccountName)
+
 		fmt.Fprintln(w, r.URL.Query().Get("name"))
 	})
 
 	fmt.Println("server will be ready to handle request...")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("listen on: ", addr)
+	http.ListenAndServe(addr, nil)
 }
